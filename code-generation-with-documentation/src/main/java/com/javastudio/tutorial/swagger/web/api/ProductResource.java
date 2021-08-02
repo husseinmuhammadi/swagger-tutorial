c650ffa032bf49685ce1ae195a41ef4a1091839f
Hossein Mohammadi <hossein.mohammadi@specific-group.com>
2021-08-02 15:47:35 +0430

    Swagger

--- code-generation-with-documentation/src/main/java/com/javastudio/tutorial/swagger/web/api/ProductResource.java
+++
@@ -1,8 +0,0 @@
-package com.javastudio.tutorial.swagger.web.api;
-
-import org.openapitools.api.ApiApi;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class ProductResource implements ApiApi {
-}
